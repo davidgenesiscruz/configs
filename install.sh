8bcbe8fdae942af5bc65b4a937eac4faaca61a06
deeeeekun <luntiang.pentakulo@gmail.com>
2018-11-11 18:49:17 +0900

    Added an installation script

---
+++ install.sh
@@ -0,0 +1,38 @@
+# Downloads
+open https://www.google.com/chrome/
+open https://www.mozilla.org/en-US/firefox/developer/
+open https://qsapp.com/download.php
+open https://bahoom.com/hyperswitch
+open https://developer.apple.com/xcode/
+open https://pqrs.org/osx/karabiner/
+
+# Installations
+## Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+## Python
+brew install python
+cd /usr/local/bin
+ln -s python3 python
+python --version
+
+## Tmux
+brew install tmux
+
+## dein.vim
+mkdir -p ~/.cache/dein
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
+
+## neovim
+pip3 install neovim
+
+## gcalcli
+git clone https://github.com/insanum/gcalcli.git
+cd gcalcli
+python setup.py install
+cd ..
+rm -rf gcalcli
+
+# Instructions
+echo 'Modify KeySetting_Default.plist within /System/Library/Input Methods/JapaneseIM.app for space and half-width space'
